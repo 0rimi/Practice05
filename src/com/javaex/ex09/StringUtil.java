d095d24d0d66a94d0d9a0249ff7fb0d72e88e9e9
이영림 <rm1113@naver.com>
2021-11-29 17:02:26 +0900

    ex09

--- src/com/javaex/ex09/StringUtil.java
+++ src/com/javaex/ex09/StringUtil.java
@@ -2,10 +2,17 @@ package com.javaex.ex09;
 
 public class StringUtil {
     
-    public static String concatString(){
-       
-        //메소드 내용작성
-        
-    }
+    
+	//필드(생략)
+
+	//메소드g/s(생략)
+	
+    //메소드 내용작성
+	
+	public static String concatString(String[] strArray){
+		return strArray[0]+strArray[1]+strArray[2];
+	}
+
+	
 
 }
--- src/com/javaex/ex09/StringUtilApp.java
+++ src/com/javaex/ex09/StringUtilApp.java
@@ -3,11 +3,16 @@ package com.javaex.ex09;
 public class StringUtilApp {
 
     public static void main(String[] args) {
-
+    	
         String[] strArray = {"SuperMan", "BatMan", "SpiderMan"};
+        //strArray 012 는 각각 슈퍼맨배트맨스파이더맨
+           
+    	//concatString이라는 메소드를 이용한값이 resultStr값이다.
+        //근데 그 메소드가 strArray값을 받아서 합치고, 출력까지 하는거.
         String resultStr = StringUtil.concatString(strArray);
         
-        System.out.println("결과 문자열:" + resultStr);
+        
+        System.out.println("결과 문자열: " + resultStr);
     }
 
 }
